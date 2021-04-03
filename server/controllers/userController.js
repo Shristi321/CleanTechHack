29243dc4ef9f6db705a0f633d9591a037009d0b3
Shristi321 <59893870+Shristi321@users.noreply.github.com>
2021-04-03 02:09:20 -0500

    Create userController.js

---
+++ server/controllers/userController.js
@@ -0,0 +1,22 @@
+var User = require('../models/user');
+var UserPost = require('../models/userPost');
+
+// Display User information.
+exports.user_information = (req, res) => {
+    res.send('Not implemented');
+};
+
+// Handle User information update on POST.
+exports.user_update_post = (req, res) => {
+    res.send('Not implemented');
+};
+
+// Display User activity history on GET.
+exports.user_activity_history_get = (req, res) => {
+    res.send('Not implemented');
+};
+
+// Display User posts on GET.
+exports.user_posts_history_get = (req, res) => {
+    res.send('Not implemented');
+};
