9a085769edf8555348ffc3d36530209075eeda99
Carolyn Wang <carolynwang@college.harvard.edu>
2021-04-03 02:56:53 -0400

    data recorded

--- cleantechhack/src/SignUpForm.js
+++ cleantechhack/src/SignUpForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
@@ -9,17 +9,78 @@ import Col from "react-bootstrap/Col"
 // figure out how to group data
 
 const SignUpForm = () => {
-    const handleSubmit = () => {
-        //endpoint stuff
+    const [data, setData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        location: {
+            city: '',
+            state: '',       
+        },
+    })
+
+    const onFirstName = ({target:{value}}) => {
+        const temp = data
+        temp.firstName = value
+        setData(temp)
+
+    }, 
+    onLastName = ({target:{value}}) => {
+        const temp = data
+        temp.lastName = value
+        setData(temp)
+
+    }, 
+    onEmail = ({target:{value}}) => {
+        const temp = data
+        temp.email = value
+        setData(temp)
+
+    }, 
+    onPassword = ({target:{value}}) => {
+        const temp = data
+        temp.password = value
+        setData(temp)
+
+    }, 
+    onCity = ({target:{value}}) => {
+        const temp = data
+        temp.location.city = value
+        setData(temp)
+
+    }, 
+    onState = ({target:{value}}) => {
+        const temp = data
+        temp.location.state = value
+        setData(temp)
+
+        console.log(temp)
+
+    }, 
+    
+
+
+    
+    handleSubmit = (e) => {
+        //endpoint stuff, send data to server
+        console.log(JSON.stringify(data))
+
     }
     return (
         <Container>
             <Form>
             <Form.Row>
-                <Col md={6}>
-                    <Form.Group controlId="formName">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" placeholder="First Last" />
+                <Col md={3}>
+                    <Form.Group controlId="formFirstName">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control onChange={onFirstName} type="text" placeholder="" />
+                    </Form.Group>
+                </Col>
+                <Col md={3}>
+                    <Form.Group controlId="formLastName">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control onChange={onLastName} type="text" placeholder="" />
                     </Form.Group>
                 </Col>
             </Form.Row> 
@@ -27,7 +88,7 @@ const SignUpForm = () => {
                 <Col>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control onChange={onEmail} type="email" placeholder="Enter email" />
                         <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                         </Form.Text>
@@ -36,27 +97,27 @@ const SignUpForm = () => {
                 <Col>
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control onChange={onPassword} type="password" placeholder="Password" />
                     </Form.Group>
                 </Col>
             </Form.Row>
             <Form.Row>
-                <Col>
+                <Col md={3}>
                     <Form.Group controlId="formCity">
                         <Form.Label>City</Form.Label>
-                        <Form.Control type="city" placeholder="" />
+                        <Form.Control onChange={onCity} type="city" placeholder="" />
                     </Form.Group>
                 
                 </Col>
-                <Col>
+                <Col md={3}>
                     <Form.Group controlId="formState">
                         <Form.Label>State</Form.Label>
-                        <Form.Control type="state" placeholder="Enter two letter state abbreviation" />
+                        <Form.Control onChange={onState} type="state" placeholder="Ex: MA" />
                     </Form.Group>
                 </Col>
             </Form.Row>
 
-            <Button style={{margin:"2vmin 0 0 0"}}onClick={handleSubmit} variant="info" type="submit">
+            <Button style={{margin:"2vmin 0 0 0"}} onClick={handleSubmit} variant="info" type="submit">
                 Submit
             </Button>
             </Form>
