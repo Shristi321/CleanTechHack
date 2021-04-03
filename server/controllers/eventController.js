2156b760f4a6c33e88e5d469192ae5c599b781d4
Carolyn Wang <carolynwang@college.harvard.edu>
2021-04-03 17:47:33 -0400

    aws

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -2,6 +2,35 @@ var User = require('../models/user');
 var Event = require('../models/Event');
 var admin  = require('../app');
 var {createDocument} = require('../firestore');
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const {ID, key} = require('../keys');
+const BUCKET_NAME = 'post-photos-cleantech';
+
+const s3 = new AWS.S3({
+    accessKeyId: ID,
+    secretAccessKey: key
+});
+
+const uploadFile = (fileName) => {
+    // Read content from the file
+    const fileContent = fs.readFileSync(fileName);
+
+    // Setting up S3 upload parameters
+    const params = {
+        Bucket: BUCKET_NAME,
+        Key: 'cat.jpg', // File name you want to save as in S3
+        Body: fileContent
+    };
+
+    // Uploading files to the bucket
+    s3.upload(params, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`File uploaded successfully. ${data.Location}`);
+    });
+};
 
 
 
