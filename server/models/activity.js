c88f4ffd13a0eccab464b5a029a558af06b09c2f
Samad Ahmed <samadahmed2019@gmail.com>
2021-04-03 09:40:44 -0500

    Create models

---
+++ server/models/activity.js
@@ -0,0 +1,7 @@
+export default function Activity(name, description, points, organizer, date) {
+    this.name = name;
+    this.description = description;
+    this.points = points;
+    this.organizer = organizer;
+    this.date = date;
+}
