66857eba2e2934f97e515d87e257baea0b08ec03
Shristi321 <59893870+Shristi321@users.noreply.github.com>
2021-04-03 02:09:14 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var dashboardRouter = require('./routes/dashboard');
+
+app.use('/dashboard', dashboardRouter);
+
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
